30d41a67faf764de529f32a9d597fcc0bc16ef53
한승재 <51234550+kimcumjee@users.noreply.github.com>
2023-02-15 22:22:27 +0900

    ui widget 추가

--- git-user-search/ViewController.swift
+++ git-user-search/ViewController.swift
@@ -7,20 +7,47 @@
 
 import UIKit
 import RxSwift
+import Then
+import SnapKit
 
 final class ViewController: UIViewController {
     
     private let viewModel = ViewModel()
+    private lazy var searchTableView = UITableView().then { view in
+        
+    }
+    
+    private lazy var searchTextField = UITextField().then { view in
+        view.attributedPlaceholder? = NSAttributedString(string: "유저 네임을 입력해주세요", attributes: [NSAttributedString.Key.foregroundColor : UIColor.black])
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setViews()
         viewModel.searchUser(name: "vex")
         viewModel.observableResult.subscribe(onNext: { data in
             print("\(data)")
         }).disposed(by: viewModel.disposeBag)
     }
+    
+    private func setViews() {
+        self.view.addSubview(searchTableView)
+        self.view.addSubview(searchTextField)
+        self.searchTextField.snp.makeConstraints {
+            $0.top.equalTo(self.view.safeAreaLayoutGuide)
+            $0.leading.equalTo(self.view.safeAreaLayoutGuide).offset(12)
+            $0.trailing.equalTo(self.view.safeAreaLayoutGuide).offset(-12)
+            $0.width.equalTo(self.view.safeAreaLayoutGuide)
+            $0.height.greaterThanOrEqualTo(64)
+        }
+        self.searchTableView.snp.makeConstraints {
+            $0.bottom.leading.trailing.equalTo(self.view.safeAreaLayoutGuide)
+            $0.top.equalTo(self.searchTextField.snp.bottom).offset(12)
+        }
+    }
+    
     deinit {
         viewModel.disposeBag = DisposeBag()
     }
 }
-
